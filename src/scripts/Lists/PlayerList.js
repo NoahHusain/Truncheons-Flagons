98a65b7f2b3afbf36d4753d46ebe783929198daa
alambrow <alexander.lambrow@gmail.com>
2021-05-10 13:21:52 -0500

    created render functions for new team and new player submit forms

---
+++ src/scripts/Lists/PlayerList.js
@@ -0,0 +1,36 @@
+// TODO: import, invoke function TeamOptionList() with HTML string for option on line 27;
+// cut and paste 27 over to TeamOptionList() function
+
+import { } from "./dataAccess.js"
+ 
+// render function for new player form
+export const newPlayerForm = () => {
+    let html = `
+    <div class="field">
+        <label class="label" for="firstName">Fist Name</label>
+        <input type="text" name="firstName" class="input"/>
+    </div>
+
+    <div class="field">
+        <label class="label" for="lastName">Last Name</label>
+        <input type="text" name="lastName" class="input"/>
+    </div>
+
+    <div class="field">
+        <label class="label" for="countryOfOrigin">Country of Origin</label>
+        <input type="text" name="countryOfOrigin" class="input"/>
+    </div>
+
+    <div class="field">
+        <label class="label" for="teamSelection">Select a team:</label>
+        <select name="teamSelection" class="dropdownList">
+            <option class="teamSelectOption" value="">--choose one!--</option>
+            <option class="teamSelectOption" value="">This will be rendered in separate module</option>
+        </select>
+    </div>
+ 
+ 
+    <button class="button" id="submitPlayer">Draft to Team</button> 
+    `
+    return html
+}
