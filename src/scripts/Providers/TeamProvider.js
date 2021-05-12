fa9cb9bb37ff00ee2cab5fd872a5c6e3f4d22648
alambrow <alexander.lambrow@gmail.com>
2021-05-12 11:20:11 -0500

    blank score card added to team api call

--- src/scripts/Providers/TeamProvider.js
+++ src/scripts/Providers/TeamProvider.js
@@ -1,4 +1,4 @@
-import { getTeams } from "../databaseAccess.js"
+import { getTeams, getScores } from "../databaseAccess.js"
 
 // render function for new team form
 export const newTeamForm = () => {
@@ -23,13 +23,28 @@ mainContainer.addEventListener("click", clickEvent => {
         // grab user input
         const inputTeamName = document.querySelector("input[name='teamName']").value
         const creationDate = new Date(Date.now()).toLocaleString("en-US")
-    
+        
+        const teamsArray = getTeams()
+        const newTeamId = teamsArray.length + 1
         const DataForAPI = {
             dateCreated: creationDate,
             name: inputTeamName,
+            id: newTeamId
         }
         // call Post API function on sendDataToAPI
         sendTeamToAPI(DataForAPI)
+
+        // create new score card to associate with team, blank at this time
+        const scoreArray = getScores()
+        const newScoreId = scoreArray.length + 1
+        const scoreCard = {
+            id: newScoreId,
+            teamId: newTeamId,
+            score: 0,
+            timestamp: Date.now()
+        }
+
+        sendScoreToAPI(scoreCard)
     }
 })
 
@@ -43,6 +58,22 @@ const sendTeamToAPI = (teamObject) => {
     }
 
     return fetch("http://localhost:8088/teams", fetchOptions)
+        .then(response => response.json())
+        .then(() => {
+            mainContainer.dispatchEvent(new CustomEvent("stateChanged"))
+        })
+}
+
+const sendScoreToAPI = (scoreObject) => {
+    const fetchOptions = {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json"
+        },
+        body: JSON.stringify(scoreObject)
+    }
+
+    return fetch("http://localhost:8088/scores", fetchOptions)
         .then(response => response.json())
         .then(() => {
             mainContainer.dispatchEvent(new CustomEvent("stateChanged"))
