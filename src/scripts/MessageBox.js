87882541ad04b98987d0f0f9b7ee09756fdb9431
Madeline Crowley <madelinecrowley@gmail.com>
2021-05-13 20:04:07 -0500

    added comments to MessageBox

--- src/scripts/MessageBox.js
+++ src/scripts/MessageBox.js
@@ -4,9 +4,13 @@ export const MessageBox = (messageText) => {
     const message = document.querySelector("#modal-message")
     message.innerHTML = messageText
     modal.style.display = "block";
+    
+    //clicking on the "x" causes message box to disappear
     span.onclick = () => {
         modal.style.display = "none";
     }
+
+    //clicking outside of the message box causes it to disappear
     window.onclick = (event) => {
         if (event.target == modal) {
             modal.style.display = "none";
